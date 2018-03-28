b0bc615b4bcfba7ba7ae774e898aefb4e41506f0
maxim <maxim218maxim218@yandex.ru>
2018-03-28 13:59:41 +0300

    Add comments about AJAX

--- app/src/main/java/com/app/maxim/mytest/MainActivity.java
+++ app/src/main/java/com/app/maxim/mytest/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.maxim.mytest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+                    Log.i("Message", ":::::::: Get answer OK :::::::::::");
                     TextView field = (TextView) findViewById(R.id.textview_1);
                     field.setText(body);
                 }
@@ -26,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    // событие при нажатии на кнопку
     public void buttonFirstClick(View view) {
+        // отправляем запрос на сервер
         QuerySender.getInstance().get("https://api-news-nin1.herokuapp.com/", listener);
     }
 
--- app/src/main/java/com/app/maxim/mytest/QuerySender.java
+++ app/src/main/java/com/app/maxim/mytest/QuerySender.java
@@ -11,26 +11,38 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+// класс для отправки запросов на сервер
 public class QuerySender {
+    // экземпляр данного класса
     private static final QuerySender INSTANCE = new QuerySender();
 
+    // метод для возврата экземпляра данного класса
     public static QuerySender getInstance() {
+        // возврат экземпляра данного класса
         return INSTANCE;
     }
 
+    // конструктор
     private QuerySender() {
+        // выводим информацию о срабатывании конструктора
         Log.i("Creating object", "----- Create new QuerySender -----");
     }
 
+    // объект для работы с интернетом
     private final OkHttpClient client = new OkHttpClient();
+    // объект для запуска
     private final Executor executor = Executors.newSingleThreadExecutor();
 
+    // метод отправки запроса на сервер
     public void get(final String url, final OnRequestCompleteListener listener) {
+        // создаём объект: запрос
         final Request request = new Request.Builder().url(url).build();
+        // добавляем слушатель к объекту запроса
         performRequest(request, listener);
     }
 
     private void performRequest(final Request request, final OnRequestCompleteListener listener) {
+        // выполняем запрос
         executor.execute(new Runnable() {
             @Override
             public void run() {
@@ -40,18 +52,24 @@ public class QuerySender {
         });
     }
 
+    // метод для получения тела ответа от сервера
     private String getBodyContent(final Request request) {
         try {
+            // если во время работы НЕ было ошибок
+            // получаем объект: ответ от сервера
             final Response response = client.newCall(request).execute();
             try (ResponseBody body = response.body()) {
                 if (response.isSuccessful() && body != null) {
+                    // возвращаем ответ от сервера в виде строки
                     return body.string();
                 }
             }
         } catch (IOException e) {
+            // если произошла ошибка во время работы, то возвращаем строку __ERROR__
             return "__ERROR__";
         }
 
+        // если произошла ошибка во время работы, то возвращаем строку __ERROR__
         return "__ERROR__";
     }
 
